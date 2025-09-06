18a32bbb46bc78cacad0465033300a6adc658ca7
safrazdev <ahmadsarfraz6760@gmil.com>
2025-09-06 12:46:07 +0530

    remove hero section bg-color

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -4,7 +4,7 @@ import { FaArrowRight } from "react-icons/fa";
 
 const Hero = () => {
   return (
-    <section id="hero" className="min-h-screen flex flex-col justify-center items-center text-center px-6 relative overflow-hidden bg-gradient-to-br from-[#002B20] via-[#013322] to-[#002B20] text-[#FFD700]">      <motion.h1
+    <section id="hero" className="min-h-screen flex flex-col justify-center items-center text-center px-6 relative overflow-hidden  text-[#FFD700]">      <motion.h1
         initial={{ y: -50, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 1 }}
